320aa9d9c9760d6a2454213c76261515e732ad88
Jacob Stanger <jstangwork@gmail.com>
2020-01-12 14:09:50 -0700

    small edits in js file.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,4 +5,27 @@
 // variables
 var usrWins = 0;
 var numGuessesRemaining = 0;
-var currentWordToGuess = 0;
+var currentWordToGuess = 'Elephant';
+
+
+
+
+// helpful code from the drink list activity
+
+// // This line of JavaScript "grabs" the main div on the page ("#drink-options");
+// var drinkDiv = document.getElementById("drink-options");
+
+// // We then use a for loop to iterate through all the drinks in drinkList.
+// // With each iteration, we perform a series of code operations each time.
+// for (var i = 0; i < drinkList.length; i++) {
+
+//   // For each drink in the array, we create a new paragraph to hold that text.
+//   // A new paragraph will be created with each iteration of the loop.
+//   var newDrinkP = document.createElement("p");
+
+//   // We then assign the the text of this paragraph to be the text in the array.
+//   newDrinkP.textContent = drinkList[i];
+
+//   // We then add the paragraph to the our main div on the page ("#drink-options")
+//   drinkDiv.appendChild(newDrinkP);
+// }
