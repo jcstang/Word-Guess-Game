0a80a25efcf0f4c470f1614e2aac5e3e70961885
Jacob Stanger <jstangwork@gmail.com>
2020-01-12 17:03:24 -0700

    moved js file to bottom of body. started game outline.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,11 +7,34 @@ var usrWins = 0;
 var numGuessesRemaining = 0;
 var currentWordToGuess = 'Elephant';
 
+// TODO: start game
+// TODO: pick a word
+// TODO: display the underscores 
+
+var currWordDiv = document.getElementById('curr-word');
+console.log('here is div: ' + document.getElementById('curr-word'));
+
+var newWord = document.createElement("p");
+newWord.textContent = currentWordToGuess;
+console.log(newWord);
+currWordDiv.appendChild(newWord);
+// newWord.textContent = currentWordToGuess;
+// currWordDiv.appendChild(newWord);
+
+
 
 document.onkeyup = function(event) {
   console.log('here is the event: ' + event);
   console.log('here is something else about event: ' + event.key);
+
+  // user has now pressed a key
   
+  // check if it was a correct guess
+    // if correct, score++ and update currentWordToGuess
+    // if wrong guess, add and display chosen letter.
+  // remove a guess available
+  // 
+
 };
 
 
