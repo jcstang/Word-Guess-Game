8ceeb5836af6ed25d523c385c8805fa022d8af3c
Jacob Stanger <jstangwork@gmail.com>
2020-01-13 09:46:17 -0700

    working on the start of the app. also figuring out taking a string to array.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,32 +6,64 @@
 // game.js
 // =========================================
 // variables
+var arrayOfWords = ['slap bracelets', 'reading rainbow', 'walkman', 'airplane', 'the blues brothers'];
 var howManyWins = 0;
 var numGuessesRemaining = 8;
-var currentWordToGuess = 'Elephant';
+//var currentWordToGuess = 'Elephant';
+var currentWord = '';
+var currentWordBlank = [];
+// var currWordToGuessArray = ['E', 'L', 'E', 'P', 'H', 'A', 'N', 'T'];
+var currWordToGuessArray = [];
 var lettersGuessed = ['A', 'Q', 'E', 'I', 'T'];
 var alphabetArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-// TODO: start game
-  // generate and 'display' a word (in underscores)
 
+// =====================
+// start of game
+// =====================
+  // pick a word from a list
+  // how many underscores are there?
+  // display that number of underscores on screen
+
+  // display the score to the DOM
+  changeTextContent('usr-win', howManyWins);
+  // display the number of guess remaining
+  changeTextContent('num-guess-remain', numGuessesRemaining);
+  //display the letters guessed
+  changeTextContent('letters-guessed', lettersGuessed);
+
+
+  // TODO: display the underscores
+    // pick a word from list
+  var wordIndexChoice = Math.floor( Math.random() * arrayOfWords.length );
+  console.log("here is wordIndexChoice: " + wordIndexChoice);
+  currentWord = arrayOfWords[ wordIndexChoice ];
+  console.log("here is currentWord: " + currentWord);
+
+  // count and fill the underscore thing
+  // string into an array
+  currWordToGuessArray = currentWord.split('');
+  console.log("here is currentWordBlank: " + currWordToGuessArray);
+  
+  for (let i = 0; i < currWordToGuessArray.length; i++) {
+    if (currWordToGuessArray[i] === ' ') {
+      console.log("hey we got a space at index: " + i);
+    }
+
+  }
+
+
+
+// ****** static version - need a looping one
 // display the current word to the DOM
-var newWord = document.createElement("p");
-newWord.textContent = currentWordToGuess;
-// append
-document.getElementById('curr-word').appendChild(newWord);
+// var newWord = document.createElement("p");
+// newWord.textContent = currentWordToGuess;
+// // append
+// document.getElementById('curr-word').appendChild(newWord);
 
-// display the score to the DOM
-// document.getElementById('usr-win').textContent = howManyWins;
-changeTextContent('usr-win', howManyWins);
 
-// display the number of guess remaining
-// document.getElementById('num-guess-remain').textContent = numGuessesRemaining;
-changeTextContent('num-guess-remain', numGuessesRemaining);
 
-//display the letters guessed
-// document.getElementById('letters-guessed').textContent = lettersGuessed;
-changeTextContent('letters-guessed', lettersGuessed)
+
 
 
 
@@ -50,6 +82,7 @@ document.onkeyup = function(event) {
   // check for valid alphabet entry
   if ( alphabetArray.includes(upperCaseEvent) ) {
     console.log('validated guess: ' + event.key);
+    //TODO: add the letter to letters guessed array. if they win, score++?
   }
   
   // check if it was a correct guess
